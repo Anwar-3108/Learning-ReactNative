9ccf7b9be5ab20e545fdc721746e5fbcc091d82a
Anwar-3108 <syed.sahab9628@gmail.com>
2024-03-24 23:25:37 +0530

    adding native  cli project

--- App.jsx
+++ App.jsx
@@ -6,7 +6,7 @@ function App(){
   return (
     <SafeAreaView>
       <View>
-        <Text>Hello World.</Text>
+        <Text>Hello World..</Text>
         <Text>Hello World</Text>
         <Text>Hello World</Text>
         <Text>Hello World</Text>
